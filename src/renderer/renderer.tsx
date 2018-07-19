766c7e813ebd843674085ba6bb6ea6274a95fb3f
wwlib <andrew@wwlib.org>
2018-07-19 10:53:59 -0400

    cleanup

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -7,22 +7,21 @@ import animate = require('pixi-animate');
 
 const findRoot = require('find-root');
 const root = findRoot(__dirname);
-const eyeSourcePath = root + '/assets/eye/eye.js';
+const eyeClassPath = root + '/assets/eye/eye.js';
 const basePath = root + '/assets/eye';
-const eyeSource: any = require(eyeSourcePath);
-console.log(eyeSource);
+const eyeClass: any = require(eyeClassPath);
 let eyeInstance: any = null;
 
 const canvasElement: HTMLCanvasElement = document.getElementById("stage") as HTMLCanvasElement;
 
-var renderer = PIXI.autoDetectRenderer(1280, 720, {
+let renderer = PIXI.autoDetectRenderer(1280, 720, {
     view: canvasElement,
     backgroundColor: 0x0,
     antialias: true
 });
 
-var stage: PIXI.Container = new PIXI.Container();
-animate.load(eyeSource.library.eye, stage, loaderCallback as any, basePath);
+let stage: PIXI.Container = new PIXI.Container();
+animate.load(eyeClass.library.eye, stage, loaderCallback as any, basePath);
 function update() {
     renderer.render(stage);
     requestAnimationFrame(update);
@@ -30,7 +29,6 @@ function update() {
 update();
 
 function loaderCallback(instance: any, loader: any):void {
-    console.log(instance, loader);
     eyeInstance = instance;
     eyeInstance.gotoAndStop('idle');
     eyeInstance.eye.eye_blue.visible = false;
